9f754c717b4d3fc9e650e94e26b290e4d14e002c
Salai Vedha Viradhan <salaivv@gmail.com>
2021-05-26 23:22:44 +0530

    Update get_centers_list.py

--- get_centers_list.py
+++ get_centers_list.py
@@ -37,7 +37,7 @@ def main():
 	site = get_request_string(args.district_id, get_todays_date())
 	hdr = {
 		'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) " \
-							"Chrome/23.0.1271.64 Safari/537.11',
+				"Chrome/23.0.1271.64 Safari/537.11',
     	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     	'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
    		'Accept-Encoding': 'none',
